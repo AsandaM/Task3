70aa705a64584e888e1f6e52b31cd14383dfab40
AsandaM <asandamehlo088@gmail.com>
2024-05-18 00:50:42 +0200

    Edited

--- script/calculator.js
+++ script/calculator.js
@@ -3,11 +3,62 @@ let input = document.querySelector('[type="text"]')
 
 let buttons = document.querySelectorAll('button')
 
-for(let button of buttons) {
-    button.addEventListener('click', ()=>{
-        console.log(button);
-        input.value += button.innerText
-    })
-    
+let currentOperation = '';
+let firstOperand = null;
+
+// Loop through each button 
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        let buttonText = button.innerText;
+
+        if (!isNaN(buttonText)) { // If the button is a number
+            input.value += buttonText;
+        } else if (buttonText === '+') { // If the button is '+'
+            firstOperand = parseFloat(input.value);
+            currentOperation = buttonText;
+            input.value = ''; // Clear the input for the next operand
+        } else if (buttonText === '-') { // If the button is '-'
+            firstOperand = parseFloat(input.value);
+            currentOperation = buttonText;
+            input.value = ''; // Clear the input for the next operand
+        } else if (buttonText === 'x') { // If the button is 'x'
+            firstOperand = parseFloat(input.value);
+            currentOperation = '*'; // Set operation to multiplication
+            input.value = ''; // Clear the input for the next operand
+        } else if (buttonText === '÷') { // If the button is '÷'
+            firstOperand = parseFloat(input.value);
+            currentOperation = '/'; // Set operation to division
+            input.value = ''; // Clear the input for the next operand
+        } else if (buttonText === '1/x') { // If the button is '1/x'
+            firstOperand = parseFloat(input.value);
+            currentOperation = '/'; // Set operation to division
+            input.value = ''; // Clear the input for the next operand
+        } else if (buttonText === '=') { // If the button is '='
+            if (currentOperation === '+') {
+                input.value = firstOperand + parseFloat(input.value);
+            } else if (currentOperation === '-') {
+                input.value = firstOperand - parseFloat(input.value);
+            } else if (currentOperation === '*') { // Check for multiplication operation
+                input.value = firstOperand * parseFloat(input.value);
+            }
+             else if (currentOperation === '/') { // Check for division operation
+                input.value = firstOperand / parseFloat(input.value);
+            }
+             else if (currentOperation === '/') { // Check for division operation
+                input.value = 1 / parseFloat(input.value);
+            }
+            currentOperation = '';
+        } else if (buttonText === 'C') { // If the button is 'C'
+            input.value = '';
+            currentOperation = '';
+            firstOperand = null;
+        }
+         else if (buttonText === 'CE') { // If the button is 'C'
+            input.value = '';
+            currentOperation = '';
+            firstOperand = null;
+        }
+    });
 }
+
 
