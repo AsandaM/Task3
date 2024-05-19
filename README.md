608c1e97c34ec48289b32c8ebcc3fe91a5263103
AsandaM <asandamehlo088@gmail.com>
2024-05-19 17:06:55 +0200

    Edited Js file

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Calculator
-
-# Please note my input when you click the operators it clears the screen but when you click equal sign it will show the output.
--- script/calculator.js
+++ script/calculator.js
@@ -1,6 +1,4 @@
 let input = document.querySelector('[type="text"]')
-// let clear = document.querySelector()
-
 let buttons = document.querySelectorAll('button')
 let clear = document.querySelectorAll('#backspace')
 
@@ -24,22 +22,22 @@ for (let button of buttons) {
             input.value = ''; // Clear the input for the next operand
         } else if (buttonText === 'x') { // If the button is 'x'
             firstOperand = parseFloat(input.value);
-            currentOperation = '*'; // Set operation to multiplication
+            currentOperation = '*'; // Set operation to *
             input.value = ''; // Clear the input for the next operand
         } else if (buttonText === '÷') { // If the button is '÷'
             firstOperand = parseFloat(input.value);
-            currentOperation = '/'; // Set operation to division
+            currentOperation = '/'; // Set operation to /
             input.value = ''; // Clear the input for the next operand
-        } else if (buttonText === '%') { // If the button is '1/x'
+        } else if (buttonText === '%') { // If the button is '%'
             input.value = parseFloat(input.value)/100
-        } else if (buttonText === '√') { // If the button is '1/x'
+        } else if (buttonText === '√') { // If the button is '√'
             input.value = Math.sqrt(parseFloat(input.value))
         } else if (buttonText === '1/x') { // If the button is '1/x'
             input.value = 1/(parseFloat(input.value))
         } else if (buttonText === '=') { // If the button is '='
-            if (currentOperation === '+') {
+            if (currentOperation === '+') { // Check for addition operation
                 input.value = firstOperand + parseFloat(input.value);
-            } else if (currentOperation === '-') {
+            } else if (currentOperation === '-') { // Check for subtraction operation
                 input.value = firstOperand - parseFloat(input.value);
             } else if (currentOperation === '*') { // Check for multiplication operation
                 input.value = firstOperand * parseFloat(input.value);
@@ -57,11 +55,11 @@ for (let button of buttons) {
             input.value = '';
             currentOperation = '';
             firstOperand = null;
-        }  else if (buttonText === ',') { // If the button is '.'
+        }  else if (buttonText === ',') { // If the button is ','
             if (!input.value.includes('.')) {
                 input.value += '.';
             }
-        } else if (buttonText === '+/-') { // If the button is '.'
+        } else if (buttonText === '+/-') { // If the button is '+/-'
             if (!input.value.includes('-')) {
                 input.value += '-';
             }
